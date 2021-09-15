e7702b0b93798eeb882fddaddb34eb60aef20314
Nikola Tomovic <ntomovic92@gmail.com>
2021-09-15 11:18:59 +0200

    Version 0.0.4

--- translized-cli.rb
+++ translized-cli.rb
@@ -1,14 +1,9 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class TranslizedCli < Formula
   desc "Translized downloads localization files"
   homepage "https://github.com/translized/translized-cli"
-  url "https://github.com/translized/translized-cli/archive/refs/tags/0.0.3.tar.gz"
-  sha256 "a3893fe7c0ad0e163300b1db3c7196dad5d56650e1e6381b1e85a62d3842e6d1"
+  url "https://github.com/translized/translized-cli/archive/refs/tags/0.0.4.tar.gz"
+  sha256 "7aef35e4f5d038dcff1730b50a45073b137f05665809413f8fd5253541b74c4d"
   license "MIT"
-
-  # depends_on "cmake" => :build
 
   def install
     libexec.install Dir["*"]
