9d71f9f85890440dfe9c518cdf0e09dd1060bc42
Nikola Tomovic <ntomovic92@gmail.com>
2021-09-17 14:37:25 +0200

    Version 0.0.5

--- translized-cli.rb
+++ translized-cli.rb
@@ -1,8 +1,8 @@
 class TranslizedCli < Formula
   desc "Translized downloads localization files"
   homepage "https://github.com/translized/translized-cli"
-  url "https://github.com/translized/translized-cli/archive/refs/tags/0.0.4.tar.gz"
-  sha256 "7aef35e4f5d038dcff1730b50a45073b137f05665809413f8fd5253541b74c4d"
+  url "https://github.com/translized/translized-cli/archive/refs/tags/0.0.5.tar.gz"
+  sha256 "77a9841e2046c2f78a5ea530cb91f5e04025c25777bd5402e2f417b44ec21018"
   license "MIT"
 
   def install
